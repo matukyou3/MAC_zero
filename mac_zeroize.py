280cbd776168af06908bf5aa7c8443a7744d9064
Kyohei Matsuiwa <kyohei.matuiwa@gmail.com>
2020-11-01 13:50:13 +0900

    rename & repair

--- mac_zeroize.py
+++ mac_zeroize.py
@@ -5,12 +5,15 @@ from scapy.all import *
 zero_mac="00:00:00:00:00:00"
 input_filename=""
 
-def zerolize_mac(in_filename,out_filename):
+def zeroize_mac(in_filename,out_filename):
     pkts=rdpcap(in_filename)
+    new_pkts =[]
     for pkt in pkts:
         pkt[Ether].src=zero_mac
         pkt[Ether].dst=zero_mac
-        wrpcap(out_filename,pkt)
+        new_pkts.append(pkt)
+    
+    wrpcap(out_filename,new_pkts)
         
 
 if __name__ =='__main__':
@@ -22,8 +25,9 @@ if __name__ =='__main__':
         exit()
     input_filename=args[1]
     if len(args)==2:
-        output_filename=input_filename+"_output"
+        out = input_filename.split('.')
+        output_filename=out + "_output.pcap"
     else:
         output_filename = args[2]
-    zerolize_mac(args[1],output_filename)
+    zeroize_mac(args[1],output_filename)
     print('Complete outputfile is ' + output_filename)
